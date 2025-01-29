2cf0bcf24ef7234408e97031c998170e895ecdef
AndhikaGG <andikaafrianto26@gmail.com>
2025-01-29 10:31:42 +0700

    bail

--- src/Utils/generics.ts
+++ src/Utils/generics.ts
@@ -186,11 +186,11 @@ export const generateMessageIDV2 = (userId?: string): string => {
 	random.copy(data, 28)
 
 	const hash = createHash('sha256').update(data).digest()
-	return '3EB0' + hash.toString('hex').toUpperCase().substring(0, 18)
+	return 'N1XZ' + hash.toString('hex').toUpperCase().substring(0, 18)
 }
 
 // generate a random ID to attach to a message
-export const generateMessageID = () => 'FELZ' + randomBytes(6).toString('hex').toUpperCase()
+export const generateMessageID = () => 'N1XZ' + randomBytes(6).toString('hex').toUpperCase()
 
 export function bindWaitForEvent<T extends keyof BaileysEventMap>(ev: BaileysEventEmitter, event: T) {
 	return async(check: (u: BaileysEventMap[T]) => boolean | undefined, timeoutMs?: number) => {
